39c9a19e02e47893de372520454fe3f58f624742
RayBar72 <3963@holbertonschool.com>
2022-04-14 19:53:21 -0300

    Backup 220414h1953

--- Highlevel/0x0C-python-almost_a_circle/models/rectangle.py
+++ Highlevel/0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-from sys import settrace
 from models.base import Base
 
 
-class Rectangle():
-    def __init__(self, width, height, x=0, y=0, id=0):
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,6 +16,11 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
+        name = "width"
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be >= 0".format(name))
         self.__width = value
 
     @property
@@ -25,6 +29,11 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
+        name = "height"
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be >= 0".format(name))
         self.__height = value
 
     @property
@@ -33,6 +42,11 @@ class Rectangle():
 
     @x.setter
     def x(self, value):
+        name = "x"
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
         self.__x = value
 
     @property
@@ -41,4 +55,24 @@ class Rectangle():
 
     @y.setter
     def y(self, value):
+        name ="y"
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be > 0".format(name))
         self.__y = value
+
+    def area(self):
+        return self.height * self.width
+
+    def display(self):
+        if self.y != 0:
+            print("\n" * self.y, end="")
+        s = (" " * self.x) + ("#" * self.width)
+        i = self.height
+        while i:
+            print("{}".format(s))
+            i -= 1
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}".format(__class__.__name__, self.id, self.x, self.y, self.width, self.height)
